e2febcae14a2de047bf28cf502c31093860a995c
Dennis Brendel <mail@dennis-brendel.de>
2021-01-27 09:30:42 +0100

    Add lambda in connect() statement
    So this can explicitly be tested

--- MainWnd.cpp
+++ MainWnd.cpp
@@ -11,6 +11,7 @@ MainWnd::MainWnd() : QWidget()
 
   auto qt4LE = new QLineEdit();
   auto qt5LE = new QLineEdit();
+  auto qt5LE2 = new QLineEdit();
   m_pLabel = new QLabel("NULL");
 
   int row = 0;
@@ -18,6 +19,8 @@ MainWnd::MainWnd() : QWidget()
   layout->addWidget(qt4LE, row, 1);
   layout->addWidget(new QLabel("Qt5:"), ++row, 0);
   layout->addWidget(qt5LE, row, 1);
+  layout->addWidget(new QLabel("Qt5-\u03BB:"), ++row, 0);
+  layout->addWidget(qt5LE2, row, 1);
   layout->addWidget(new QLabel("Result:"), ++row, 0);
   layout->addWidget(m_pLabel, row, 1);
 
@@ -25,5 +28,6 @@ MainWnd::MainWnd() : QWidget()
 
   connect(qt4LE, SIGNAL(textChanged(const QString&)), m_pLabel, SLOT(setText(const QString&)));
   connect(qt5LE, &QLineEdit::textChanged, m_pLabel, &QLabel::setText);
+  connect(qt5LE2, &QLineEdit::textChanged, [this, qt5LE2](){m_pLabel->setText(qt5LE2->text());});
 }
 
